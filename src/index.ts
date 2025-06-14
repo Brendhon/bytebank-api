a569a59d9d9dadee0edf160f10d60f418803fa33
Brendhon <brendhon.e.c.m@gmail.com>
2025-06-14 15:07:41 -0300

    refactor: improve code readability and organization in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,34 @@ import { resolvers } from './resolvers';
 import { typeDefs } from './schema';
 
 const startServer = async () => {
+  // Create an instance of Express
   const app = express();
 
+  // Create an instance of ApolloServer with type definitions and resolvers
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
 
+  // Start the Apollo Server
   await server.start();
 
   app.use(
-    '/graphql',
-    cors(),
-    bodyParser.json(),
-    expressMiddleware(server, {
+    '/graphql', // Path to your GraphQL endpoint
+    cors(), // Enable CORS for all origins
+    bodyParser.json(), // Parse JSON bodies
+    expressMiddleware(server, { // Middleware to handle GraphQL requests
+      // Optional: You can pass a context function to provide additional data to resolvers
+      // For example, you can pass the request headers or user information
+      // Here we pass the authorization token from headers
       context: async ({ req }) => ({
         token: req.headers.authorization,
       }),
     })
   );
 
-  app.listen(3000, () => {
-    console.log('🚀 Server ready at http://localhost:3000/graphql');
-  });
+  // Start the Express server
+  app.listen(3000, () => console.log('🚀 Server ready at http://localhost:3000/graphql'));
 };
 
 startServer();
